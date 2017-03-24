4c13753b96ab7c1325e1ae39c1f4c8a3d1c455e8
skolobukhov <skolobuhov@gmail.com>
2017-03-25 01:11:38 +0500

    end_point

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        var rect = new Rect(Screen.width / 2f - 50, Screen.height / 2f - 15, 100, 30);
+
+        if (GUI.Button(rect, "Play"))
+        {
+            SceneManager.LoadScene("test");
+        }
+    }
+}
