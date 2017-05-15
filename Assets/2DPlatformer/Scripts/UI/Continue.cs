e4fea3af748f2bfe423d2cd8980cac9e5a21b0f2
ElizavetaAlekseevna <lizka030697@mail.ru>
2017-05-15 13:02:02 +0500

    Saving

---
+++ Assets/2DPlatformer/Scripts/UI/Continue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+
+public class Continue : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        int number_lv;
+        FileStream fs = new FileStream("saved.txt", FileMode.OpenOrCreate);
+        StreamReader sr = new StreamReader(fs);
+        if (!sr.EndOfStream)
+        {
+            number_lv = Convert.ToInt32(sr.ReadLine());
+        }
+        else number_lv = 1;
+        sr.Close();
+        fs.Close();
+        SceneManager.LoadScene(number_lv);
+    }
+}
