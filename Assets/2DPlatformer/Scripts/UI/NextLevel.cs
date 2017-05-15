de9cd7ca1389bae8420e1855bb0c7af0792ee741
ElizavetaAlekseevna <lizka030697@mail.ru>
2017-05-15 12:59:51 +0500

    Saving

--- Assets/2DPlatformer/Scripts/UI/NextLevel.cs
+++ Assets/2DPlatformer/Scripts/UI/NextLevel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
+using System;
 
 public class NextLevel : MonoBehaviour
 {
@@ -11,5 +13,29 @@ public class NextLevel : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         SceneManager.LoadScene(levelNumber);
+        FileStream fs = new FileStream("saved.txt",FileMode.OpenOrCreate);
+        StreamReader sr = new StreamReader(fs);
+        if (!sr.EndOfStream)
+        {
+            int filelevel = Convert.ToInt32(sr.ReadLine());
+            sr.Close();
+            fs.Close();
+            if (levelNumber > filelevel)
+                Write_num();
+        }
+        else
+        {
+            sr.Close();
+            fs.Close();
+            Write_num();
+        }    
+    }
+    void Write_num()
+    {
+        FileStream fs = new FileStream("saved.txt", FileMode.Truncate);
+        StreamWriter sw = new StreamWriter(fs);
+        sw.Write(levelNumber);
+        sw.Close();
+        fs.Close();
     }
 }
